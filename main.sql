849578e1e0e2e7b16aabe24bf97b588b6b315e91
Tim Chan <tim@chan.net>
2021-02-23 19:15:15 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,22 @@
+.header on
+.mode column
+
+create table fam_tree(
+  id integer,
+  name TEXT,
+  parent_id integer
+);
+
+insert into fam_tree values
+  (1, 'Root A', null),
+  (2, 'Root B', null),
+  (3, 'Child A1', 1),
+  (4, 'Child A2', 1),
+  (5, 'Child B1', 2),
+  (6, 'Child B2', 2),
+  (7, 'Grandchild A1a', 3),
+  (8, 'Grandchild A1b', 3)
+ ;
+
+
+  select * from fam_tree 
